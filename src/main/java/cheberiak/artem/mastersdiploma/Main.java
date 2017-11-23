c8a0e263296e372c6bca89d8ec6476f6526a1ebc
ace <ace@melexis.com>
2017-11-24 01:05:30 +0200

    Added interpolation method

--- src/main/java/cheberiak/artem/mastersdiploma/Main.java
+++ src/main/java/cheberiak/artem/mastersdiploma/Main.java
@@ -9,6 +9,8 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     private static Logger logger = Logger.getLogger(Main.class);
@@ -24,18 +26,27 @@ public class Main {
                 URL oddFieldURL = Main.class.getClassLoader()
                                             .getResource(commandLine.getOptionValue(
                                                     CliHandler.ODD_FIELD_PATH_OPTION_STRING));
-                result = deinterlaceLineDuplication(loadImage(oddFieldURL), false);
+                result = deinterlaceLineInterpolation(loadImage(oddFieldURL), false);
             } else if (commandLine.hasOption(CliHandler.EVEN_FIELD_PATH_OPTION_STRING)) {
                 URL oddFieldURL = Main.class.getClassLoader()
                                             .getResource(commandLine.getOptionValue(
                                                     CliHandler.EVEN_FIELD_PATH_OPTION_STRING));
-                result = deinterlaceLineDuplication(loadImage(oddFieldURL), true);
+                result = deinterlaceLineInterpolation(loadImage(oddFieldURL), true);
             }
-            
+
             writeImage(result, commandLine.getOptionValue(CliHandler.RESULT_OPTION_STRING));
         } catch (Exception e) {
             logger.error(e);
         }
+    }
+
+    private static void writeImage(BufferedImage img, String pathname) throws IOException {
+        String[] split = pathname.split("\\.");
+        ImageIO.write(img, split[split.length - 1], new File(pathname));
+    }
+
+    private static BufferedImage loadImage(URL resource) throws IOException {
+        return ImageIO.read(resource);
     }
 
     private static BufferedImage deinterlaceLineDuplication(BufferedImage field, boolean isFieldEven) {
@@ -45,9 +56,9 @@ public class Main {
 
         if (isFieldEven) {
             for (int lineIndex = 1; lineIndex < field.getHeight(); lineIndex += 2) {
-                BufferedImage oddLine = field.getSubimage(0, lineIndex, field.getWidth(), 1);
-                returnValueGraphics.drawImage(oddLine, 0, lineIndex - 1, null);
-                returnValueGraphics.drawImage(oddLine, 0, lineIndex, null);
+                BufferedImage evenLine = field.getSubimage(0, lineIndex, field.getWidth(), 1);
+                returnValueGraphics.drawImage(evenLine, 0, lineIndex - 1, null);
+                returnValueGraphics.drawImage(evenLine, 0, lineIndex, null);
             }
         } else {
             for (int lineIndex = 0; lineIndex < field.getHeight(); lineIndex += 2) {
@@ -62,12 +73,113 @@ public class Main {
         return returnValue;
     }
 
-    private static void writeImage(BufferedImage img, String pathname) throws IOException {
-        String[] split = pathname.split("\\.");
-        ImageIO.write(img, split[split.length - 1], new File(pathname));
+    private static BufferedImage deinterlaceLineInterpolation(BufferedImage field, boolean isFieldEven) {
+        BufferedImage returnValue =
+                new BufferedImage(field.getWidth(), field.getHeight(), field.getType());
+        Graphics returnValueGraphics = returnValue.getGraphics();
+
+        if (isFieldEven) {
+            returnValueGraphics.drawImage(field.getSubimage(0, 0, field.getWidth(), 1),
+                                          0,
+                                          0,
+                                          null);
+            returnValueGraphics.drawImage(field.getSubimage(0, 1, field.getWidth(), 1),
+                                          0,
+                                          1,
+                                          null);
+
+            for (int lineIndex = 2; lineIndex < field.getHeight(); lineIndex += 2) {
+                BufferedImage evenLineHigher = field.getSubimage(0, lineIndex - 1, field.getWidth(), 1);
+                BufferedImage evenLineLower = field.getSubimage(0, lineIndex + 1, field.getWidth(), 1);
+                returnValueGraphics.drawImage(getInterpolatedLine(evenLineHigher, evenLineLower),
+                                              0,
+                                              lineIndex,
+                                              null);
+                returnValueGraphics.drawImage(field.getSubimage(0, lineIndex + 1, field.getWidth(), 1),
+                                              0,
+                                              lineIndex + 1,
+                                              null);
+            }
+        } else {
+            returnValueGraphics.drawImage(field.getSubimage(0, 0, field.getWidth(), 1),
+                                          0,
+                                          0,
+                                          null);
+            returnValueGraphics.drawImage(field.getSubimage(0, field.getHeight() - 1, field.getWidth(), 1),
+                                          0,
+                                          field.getHeight() - 1,
+                                          null);
+
+            for (int lineIndex = 1; lineIndex < field.getHeight() - 1; lineIndex += 2) {
+                BufferedImage evenLineHigher = field.getSubimage(0, lineIndex - 1, field.getWidth(), 1);
+                BufferedImage evenLineLower = field.getSubimage(0, lineIndex + 1, field.getWidth(), 1);
+                returnValueGraphics.drawImage(getInterpolatedLine(evenLineHigher, evenLineLower),
+                                              0,
+                                              lineIndex,
+                                              null);
+                returnValueGraphics.drawImage(field.getSubimage(0, lineIndex + 1, field.getWidth(), 1),
+                                              0,
+                                              lineIndex + 1,
+                                              null);
+            }
+        }
+
+        returnValueGraphics.dispose();
+
+        return returnValue;
     }
 
-    private static BufferedImage loadImage(URL resource) throws IOException {
-        return ImageIO.read(resource);
+    private static BufferedImage getInterpolatedLine(BufferedImage oddLineHigher,
+                                                     BufferedImage oddLineLower) {
+        BufferedImage returnValue = new BufferedImage(oddLineHigher.getWidth(), 1, oddLineHigher.getType());
+        for (int pixelIndex = 1; pixelIndex < returnValue.getWidth() - 2; pixelIndex++) {
+            returnValue.setRGB(pixelIndex,
+                               0,
+                               getInterpolatedPixel(oddLineHigher, oddLineLower, pixelIndex));
+        }
+
+        returnValue.setRGB(0, 0, getIterpolatedPixelFromPixelList(
+                Arrays.asList(new Color(oddLineHigher.getRGB(0, 0), true),
+                              new Color(oddLineHigher.getRGB(1, 0), true),
+                              new Color(oddLineLower.getRGB(0, 0), true),
+                              new Color(oddLineLower.getRGB(1, 0), true))));
+        returnValue.setRGB(returnValue.getWidth() - 1, 0, getIterpolatedPixelFromPixelList(
+                Arrays.asList(new Color(oddLineHigher.getRGB(returnValue.getWidth() - 1, 0), true),
+                              new Color(oddLineHigher.getRGB(returnValue.getWidth() - 2, 0), true),
+                              new Color(oddLineLower.getRGB(returnValue.getWidth() - 1, 0), true),
+                              new Color(oddLineLower.getRGB(returnValue.getWidth() - 2, 0), true))));
+        return returnValue;
+    }
+
+    private static int getInterpolatedPixel(BufferedImage oddLineHigher, BufferedImage oddLineLower, int pixelIndex) {
+        List<Color> neighborPixels = Arrays.asList(
+                new Color(oddLineHigher.getRGB(pixelIndex - 1, 0), true),
+                new Color(oddLineHigher.getRGB(pixelIndex, 0), true),
+                new Color(oddLineHigher.getRGB(pixelIndex + 1, 0), true),
+                new Color(oddLineLower.getRGB(pixelIndex - 1, 0), true),
+                new Color(oddLineLower.getRGB(pixelIndex, 0), true),
+                new Color(oddLineLower.getRGB(pixelIndex + 1, 0), true));
+        return getIterpolatedPixelFromPixelList(neighborPixels);
+    }
+
+    private static int getIterpolatedPixelFromPixelList(List<Color> neighborPixels) {
+        int r = 0;
+        int g = 0;
+        int b = 0;
+        int a = 0;
+
+        for (Color pixel : neighborPixels) {
+            r += pixel.getRed();
+            g += pixel.getGreen();
+            b += pixel.getBlue();
+            a += pixel.getAlpha();
+        }
+
+        r /= neighborPixels.size();
+        g /= neighborPixels.size();
+        b /= neighborPixels.size();
+        a /= neighborPixels.size();
+
+        return new Color(r, g, b, a).getRGB();
     }
 }
