7776491690c28514cb629c877d1f38ef6c93733e
ace <ace@melexis.com>
2017-11-26 18:09:22 +0200

    Enhanced CLI output

--- src/main/java/cheberiak/artem/mastersdiploma/Main.java
+++ src/main/java/cheberiak/artem/mastersdiploma/Main.java
@@ -5,9 +5,10 @@ import org.apache.log4j.Logger;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.function.BiFunction;
 
 import static cheberiak.artem.mastersdiploma.CliHandler.*;
@@ -27,37 +28,44 @@ public class Main {
             boolean isFieldEven = false;
             BufferedImage source = null;
             BiFunction<BufferedImage, Boolean, BufferedImage> algorithm;
+            logger.info("Starting deintrelacing...");
 
             if (commandLine.hasOption(INTERPOLATOIN_ALGORITHM_OPTION_STRING)) {
                 algorithm = DEINTERLACE_LINE_INTERPOLATION;
+                logger.info("Line interpolation algorithm is chosen.");
             } else {
                 algorithm = DEINTERLACE_LINE_DUPLICATION;
+                logger.info("Line duplication algorithm is chosen by default.");
             }
 
             if (commandLine.hasOption(ODD_FIELD_PATH_OPTION_STRING)) {
                 source = loadImage(Main.class.getClassLoader()
                                              .getResource(commandLine.getOptionValue(
                                                      ODD_FIELD_PATH_OPTION_STRING)));
+                logger.info("Interpolating odd field...");
             } else if (commandLine.hasOption(EVEN_FIELD_PATH_OPTION_STRING)) {
                 source = loadImage(Main.class.getClassLoader()
                                              .getResource(commandLine.getOptionValue(
                                                      EVEN_FIELD_PATH_OPTION_STRING)));
                 isFieldEven = true;
+                logger.info("Interpolating even field...");
             } else {
                 logger.error("No field received! Nothing to convert");
                 System.exit(1);
             }
-            
-            writeImage(algorithm.apply(source, isFieldEven), commandLine.getOptionValue(RESULT_OPTION_STRING));
+
+            Path resultPath = Paths.get(commandLine.getOptionValue(RESULT_OPTION_STRING));
+            writeImage(algorithm.apply(source, isFieldEven), resultPath);
+            logger.info("Image deinterlaced. Path: " + resultPath);
         } catch (Exception e) {
             logger.error(e);
             System.exit(1);
         }
     }
 
-    private static void writeImage(BufferedImage img, String pathname) throws IOException {
-        String[] split = pathname.split("\\.");
-        ImageIO.write(img, split[split.length - 1], new File(pathname));
+    private static void writeImage(BufferedImage img, Path path) throws IOException {
+        String[] split = path.getFileName().toString().split("\\.");
+        ImageIO.write(img, split[split.length - 1], path.toFile());
     }
 
     private static BufferedImage loadImage(URL resource) throws IOException {
