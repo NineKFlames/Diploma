cafc507ecd8c6ff10a7d116ca0a849407eeac556
ace <ace@melexis.com>
2017-11-26 17:33:01 +0200

    Added log4j properties and moved algorithms to separate classes

---
+++ src/main/java/cheberiak/artem/mastersdiploma/LineDuplication.java
@@ -0,0 +1,30 @@
+package cheberiak.artem.mastersdiploma;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class LineDuplication {
+    public static BufferedImage deinterlaceLineDuplication(BufferedImage field, boolean isFieldEven) {
+        BufferedImage returnValue =
+                new BufferedImage(field.getWidth(), field.getHeight(), field.getType());
+        Graphics returnValueGraphics = returnValue.getGraphics();
+
+        if (isFieldEven) {
+            for (int lineIndex = 1; lineIndex < field.getHeight(); lineIndex += 2) {
+                BufferedImage evenLine = field.getSubimage(0, lineIndex, field.getWidth(), 1);
+                returnValueGraphics.drawImage(evenLine, 0, lineIndex - 1, null);
+                returnValueGraphics.drawImage(evenLine, 0, lineIndex, null);
+            }
+        } else {
+            for (int lineIndex = 0; lineIndex < field.getHeight(); lineIndex += 2) {
+                BufferedImage oddLine = field.getSubimage(0, lineIndex, field.getWidth(), 1);
+                returnValueGraphics.drawImage(oddLine, 0, lineIndex, null);
+                returnValueGraphics.drawImage(oddLine, 0, lineIndex + 1, null);
+            }
+        }
+
+        returnValueGraphics.dispose();
+
+        return returnValue;
+    }
+}
